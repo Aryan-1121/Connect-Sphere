0fd2d1de19a627adff1c203b4a9ba68c12eee913
Aryan <aryanmaurya879@gmail.com>
2024-03-23 14:10:44 +0530

    defined routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,15 @@ const App = () => {
         <Route element= {<RootLayout />}>
 
             <Route index element={<Home />} /> 
-        
+            <Route path='/explore' element={<Element />} />
+            <Route path='/profile/:id/*' element={<Profile />} />
+            <Route path='/create-post' element={<CreatePost />} />
+            <Route path='/saved' element={<Saved />} />
+            <Route path='/posts/:id' element={<PostDetails />} />
+            <Route path='/all-users' element={<AllUsers />} />
+            <Route path='/update-post/:id' element={<EditPost />} />
+            <Route path='/update-profile/:id' element={<UpdateProfile />} />
+
         </Route>
 
 
