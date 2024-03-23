680e4faf7d3fdfb5a040968b5c3587664a81b677
Aryan <aryanmaurya879@gmail.com>
2024-03-23 13:35:50 +0530

    added consts to export sidebar and bottomBar url,routes,labels

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -36,11 +36,15 @@ const LeftSidebar = () => {
                 <p className="body-bold">
                   {user.name}
                 </p>
-                <p className="small-regular text-light-3" >
+                <p className=" small-regular text-light-3" >
                   @{user.username}
                 </p>
               </div>
           </Link>
+
+          <ul className="flex flex-col gap-6">
+            
+          </ul>
 
       </div>
     </nav>
